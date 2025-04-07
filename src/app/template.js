97f2d40d0e799d75407852545709e47a9e24a1ff
Tom CHARLET <charlettom11@gmail.com>
2025-04-07 23:13:46 +0200

    update

--- src/app/template.js
+++ src/app/template.js
@@ -6,7 +6,7 @@ const template = ({ children }) => {
     return <main className="w-full h-dvh bg-black overflow-hidden">
         <div className="w-full max-w-[393px] h-full max-h-[852px] bg-grey-900 mx-auto grid grid-rows-[auto_1fr]">
             {children}
-            <Menu />
+            {/* <Menu /> */}
         </div>
     </main>
 }
