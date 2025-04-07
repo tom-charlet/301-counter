ff120d4a0a1e7a38f4ef867fe55381fe0e48fa57
Tom CHARLET <charlettom11@gmail.com>
2025-04-07 23:11:29 +0200

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 
 const Menu = () => {
-    return <div className="p-4">
-        <div className="h-[60px] p-1.5 bg-white rounded-xl">
+    return <div className="p-4 w-full fixed left-0 bottom-0">
+        <div className="h-[60px] p-1.5 bg-white rounded-xl border border-grey-300 max-w-[calc(393px-32px)] mx-auto">
 
         </div>
     </div>
