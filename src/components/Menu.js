3b0e15631002671e27e6d1fd85ebe90a98e705e0
Tom CHARLET <charlettom11@gmail.com>
2025-04-07 23:16:58 +0200

    upddate

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,11 @@
 
 const Menu = () => {
-    return <div className="p-4 w-full fixed left-0 bottom-0">
-        <div className="h-[60px] p-1.5 bg-white rounded-xl border border-grey-300 max-w-[calc(393px-32px)] mx-auto">
+    return null
+    // return <div className="p-4 w-full fixed left-0 bottom-0">
+    //     <div className="h-[60px] p-1.5 bg-white rounded-xl border border-grey-300 max-w-[calc(393px-32px)] mx-auto">
 
-        </div>
-    </div>
+    //     </div>
+    // </div>
 }
 
 export default Menu
